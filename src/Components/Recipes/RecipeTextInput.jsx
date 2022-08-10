17eb89b36420ab2769558b5559f5e521fe136bcd
Demnu <harrison.joe.collins@gmail.com>
2022-08-10 23:31:42 +1000

    RecipeTextInput styling changes

--- src/Components/Recipes/RecipeTextInput.jsx
+++ src/Components/Recipes/RecipeTextInput.jsx
@@ -2,7 +2,7 @@ import React from "react";
 const RecipeTextInput = ({ value }) => {
   return (
     <input
-      className="bg-white border focus:outline-none  rounded-md p-3 w-48 "
+      className="bg-white border hover:border-blue-300 focus:border-blue-500 border-gray-300 focus:outline-none rounded-md p-3 w-48 "
       value={value}
     />
   );
